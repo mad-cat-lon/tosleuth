cace0b9ac6afbd4fba5011ebaf78fa6f20a7a445
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-12-19 20:15:34 -0500

    Extension now has a frontend and sends urls to server

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,19 +1,54 @@
 browser.runtime.onInstalled.addListener(() => {
     console.log("Extension initialized.")
 })
+// const seen = [];
+// // Listen for any tab updates
+// browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     // Check if the tab's status is 'complete' which means the page and its scripts have loaded
+//     console.log(seen);
+//     if (changeInfo.status === 'complete' && tab.active && tab.highlighted) {
+//       let domain = (new URL(tab.url)).hostname;
+//       console.log(domain);
+//       if (!(seen.includes(domain))) {
+//         // Inject the content script into the current page
+//         browser.tabs.executeScript(tabId, { file: 'content.js' });
+//         seen.push(domain)
+//       }
+//       else {
+//         console.log('Skipping domain');
+//       }
+//   }
+// });
 
-// Listen for any tab updates
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Check if the tab's status is 'complete' which means the page and its scripts have loaded
-    if (changeInfo.status === 'complete' && tab.active) {
-      // Inject the content script into the current page
-      browser.tabs.executeScript(tabId, { file: 'extension\\js\\content.js' }).then(
-        (message) => {
-          console.log("Success: " + message);
-        },
-        (message) => {
-          console.log("Failure: " + message);
-        }
-      );
-    }
+
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'download') {
+    // Create a new file blob with the text data
+    const blob = new Blob([message.data], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    // Use the downloads API to download the file
+    browser.downloads.download({
+      url: url,
+      filename: message.filename.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.txt'
+    });
+  }
+  if (message.action === 'retrieveContent') {
+    const url = 'http://127.0.0.1:8000/add_from_url';
+    // send it to our backend server
+    fetch(url, {
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 'url': message.source })
+    })
+    .then(response => response.json()) // Parse JSON response
+    .then(data => {
+      console.log("Response from backend received: ", data);
+    })
+    .catch(error => {
+      console.log("Error in fetching: ", error);
+    });
+  }
 });
--- extension/js/content.js
+++ extension/js/content.js
@@ -1,23 +1,26 @@
 function findLegalLinks() {
-  console.log("hi");
     const links = Array.from(document.links);
     const legalLinks = links.filter(link => {
-      return /terms|privacy|legal|tos|conditions|policy/i.test(link.href);
+      return /terms|privacy|legal|policy/i.test(link.href);
     });
-    // console.log("hi");
-    // legalLinks.forEach(link => {
-    //   // Fetch and download the content
-    //   fetch(link.href)
-    //     .then(response => response.text())
-    //     .then(text => {
-    //       // Here you would normally save the content, but for demonstration, we'll just log it
-    //       console.log(text);
-    //       // To download the file, we'll send a message to the background script
-    //       // browser.runtime.sendMessage({ action: 'download', data: text, filename: link.innerText });
-    //     })
-    //     .catch(console.error);
-    //     console.log(link.href);
-    // });
+    legalLinks.forEach(link => {
+      // Fetch and download the content
+      fetch(link.href)
+        .then(response => response.text())
+        .then(text => {
+          // Just send the URL to the backend so we can handle it for now
+          browser.runtime.sendMessage({
+            action: 'retrieveContent',
+            data: text,
+            source: link.href
+          })
+          // Send message to background.js so we download the file 
+          // Not downloading from within Firefox rn, let the backend handle it so we can render js
+          // browser.runtime.sendMessage({ action: 'download', data: text, filename: link.href });
+        })
+        .catch(console.error);
+        console.log(link.href);
+    });
   }
     
 // Run the function when the content script is loaded
