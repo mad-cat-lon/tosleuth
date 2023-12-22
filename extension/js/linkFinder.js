fc21a33dd37775c238b5d3c90d3d02a867a76e30
b0ltzm4nn <113548315+xor-eax-eax-ret@users.noreply.github.com>
2023-12-22 12:29:27 -0500

    Added ability to send current page HTML to backend

--- extension/js/linkFinder.js
+++ extension/js/linkFinder.js
@@ -7,25 +7,27 @@ function findLegalLinks() {
     let legalLinks = links.filter(link => {
       return /terms|privacy|legal|policy/i.test(link.href);
     });
-    legalLinks = removeDuplicates(legalLinks);
+    console.log(legalLinks);
+    legalLinks = legalLinks.map(link => link.href)
+    legalLinks = removeDuplicates(legalLinks)
     console.log(legalLinks);
     legalLinks.forEach(link => {
       // Fetch and download the content
-      fetch(link.href)
+      fetch(link)
         .then(response => response.text())
         .then(text => {
           // Just send the URL to the backend so we can handle it for now
           browser.runtime.sendMessage({
             action: 'retrieveContent',
             data: text,
-            source: link.href
+            source: link
           })
           // Send message to background.js so we download the file 
           // Not downloading from within Firefox rn, let the backend handle it so we can render js
           // browser.runtime.sendMessage({ action: 'download', data: text, filename: link.href });
         })
         .catch(console.error);
-        console.log(link.href);
+        console.log(link);
     });
   }
     
