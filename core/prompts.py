2d7710e93c0f21e4d46f7f80065bb5dea637267c
mad-cat-lon <113548315+mad-cat-lon@users.noreply.github.com>
2024-01-22 17:57:50 -0500

    tweak prompt

--- core/prompts.py
+++ core/prompts.py
@@ -2,11 +2,9 @@ from langchain.prompts import StringPromptTemplate
 from pydantic import BaseModel, validator
 
 PROMPT = """
-<s>[INST] <<SYS>>
-You are a helpful AI assistant who analyzes legal clauses in a website's terms of service and privacy agreements. Given a legal statement and 4 pieces of text extracted from the document, pick the one that answers the statement best and choose the number. Some of the statements may not directly answer the statement or be relevant. If no texts are satisfactory, choose 0. Output a valid JSON object containing the choice of text and your reasoning, but keep it concise. DO NOT OUTPUT ANYTHING OTHER THAN THE JSON OBJECT. YOU MUST ONLY OUTPUT JSON OR YOU WILL BE PUNISHED.
+<s>[INST]
+You are a helpful AI assistant who analyzes legal clauses in a website's terms of service and privacy agreements. Given a legal statement and 4 pieces of text extracted from the document, pick the one that answers the statement best and choose the number. Some of the statements may not directly answer the statement or they may be irrelevant. If no texts are satisfactory, choose 0. Output a valid JSON object containing the choice of text and your reasoning, but keep it concise. DO NOT OUTPUT ANYTHING OTHER THAN THE JSON OBJECT. "You" refers to the user, and "we" or "us" refer to the service. 
 Here are a few examples.
-
-## USER INPUT:
 Given the statement "You sign away all moral rights", which of the following texts, if any, answer it?
 
 1) "you irrevocably waive any claims and assertions of moral rights or attribution with respect to Your Content."
@@ -17,16 +15,14 @@ Given the statement "You sign away all moral rights", which of the following tex
 
 4) "By submitting Your Content to the Services, you represent and warrant that you have all rights, power, and authority necessary to grant the rights to Your Content contained within these Terms. Because you alone are responsible for Your 
 Content, you may expose yourself to liability if you post or share Content without all necessary rights."
-
-## MODEL OUTPUT:
+[/INST]
 {{
     "choice": 1,
     "reason": "This text explicitly mentions that the user waives moral rights with respect to their content, which aligns with the statement. The other texts do not directly address moral rights and are unrelated to the given statement."
 }}
-
-<</SYS>>
-
-Given the statement "{query}", which of the following texts, if any, answer it? Think carefully. 
+</s>
+[INST]
+Given the statement "{query}", which of the following texts, if any, answer it? Think carefully and provide the output in JSON. 
 
 1) {result1}
 
@@ -36,7 +32,6 @@ Given the statement "{query}", which of the following texts, if any, answer it? 
 
 4) {result4}
 [/INST]
-    
 """
 
 n_results = 4
