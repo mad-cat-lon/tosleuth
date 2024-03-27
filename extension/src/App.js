cbc5ecd3ec01f2dd2ebe978246619948dc84a731
mad-cat-lon <113548315+mad-cat-lon@users.noreply.github.com>
2024-03-27 14:38:42 -0400

    Added test footer

--- extension/src/App.js
+++ extension/src/App.js
@@ -297,6 +297,11 @@ function App() {
           ))}
         </div>
       </div>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        </aside>
+      </footer>
     </div>
   );
 }
